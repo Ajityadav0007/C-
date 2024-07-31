5675412c7efcaf4b081fdad3f3df4206e5e88b58
ajit yadav <aj1221yadav@gmail.com>
2024-07-31 07:10:43 -0700

    NonGenric Collection

---
+++ DictnaoryCollection.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class pro123
+
+    {
+        public static void Main()
+        {
+            DictnaoryCollection d = new DictnaoryCollection();
+        }
+
+        internal class DictnaoryCollection
+        {
+            public DictnaoryCollection()
+            {
+                Console.WriteLine("****Dictonary Implimentation******");
+                Dictionary<string, int> n = new Dictionary<string, int>();
+
+                n.Add("Ajit", 101);
+                n.Add("Amit", 102);
+                n.Add("Adi", 103);
+
+                Console.WriteLine("All Element In Dictonary");
+
+                foreach (KeyValuePair<string, int> item in n)
+                {
+
+                    Console.WriteLine($"{item.Key}:{item.Value}");
+
+                }
+
+
+
+                object k = n.Remove("Ajit");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Updated Dictonary After Remove Operation Perform:=");
+                foreach (KeyValuePair<string, int> item1 in n)
+                {
+                    Console.WriteLine(item1);
+
+                }
+
+
+
+                Console.WriteLine();
+
+
+                var Filter = n.Where(x => x.Key.Contains("Ajit")).ToDictionary(x => x.Key, x => x.Value);
+
+                Console.WriteLine("Filter Dictonary=");
+
+                foreach (var pair in n)
+                {
+
+                    Console.WriteLine($"key={pair.Key},value={pair.Value}");
+
+
+
+                }
+
+                n.Clear();
+
+                Console.WriteLine();
+
+
+                if (n.Count == 0)
+                {
+
+                    Console.WriteLine("Dictory IsEmpty=True");
+
+                }
+                else { Console.WriteLine("Dictory IsEmpty=False"); }
+
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
---
+++ StackCollection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class StackCollection
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("******** Genric Stack Implimentation************");
+        Stack<int> stack = new Stack<int>();
+            
+
+           stack.Push(101);
+            stack.Push(102);
+            stack.Push(103);
+            stack.Push(104);
+            stack.Push(105);
+
+            Console.WriteLine("All Stack Value=");
+
+            foreach (int i in stack)
+            { 
+            
+            Console.WriteLine(i);
+            
+            }
+
+           
+
+            object t= stack.Pop();
+            object y= stack.Pop();
+            object z= stack.Pop();
+            object m= stack.Pop();
+            object k = stack.Pop();
+        
+
+            Console.WriteLine($"Pooped Element..........=\n{t}\n{y}\n{z}\n{m}\n{k}");
+            
+
+
+
+            if (stack.Count == 0)
+            {
+
+                Console.WriteLine("Stack Is Empty");
+
+            }
+            else
+            { 
+            
+            Console.WriteLine("stack is not Empty");
+            
+            }
+
+        }
+
+    }
+}
