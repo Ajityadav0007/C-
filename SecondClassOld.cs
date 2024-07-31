4801851e3d017cc6ad062a834d7f417c36b006ba
ajit yadav <aj1221yadav@gmail.com>
2024-07-31 07:06:03 -0700

    ExtensionMethod

---
+++ SecondClassOld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+   public static class SecondClassOld
+    {
+        public static void Sub(this FirstClassOld old,int y,int z)
+        {
+            Console.WriteLine($"Substration={old.x-y -z}");
+        
+        
+        }
+        public static void multi(this FirstClassOld old, int y, int z)
+        {
+            Console.WriteLine($"Multiplication={old.x * y * z}");
+
+
+        }
+        public static void div(this FirstClassOld old, int y, int z)
+        {
+            Console.WriteLine($"Division={old.x / y / z}");
+
+
+        }
+
+    }
+}
---
+++ ThidClassCall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class ThidClassCall
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("******* Extension Method*******");
+
+            FirstClassOld k = new FirstClassOld();
+            k.Add(10);
+            k.Sub(10,2);
+            k.multi(11,2);
+            k.div(11,3);
+
+        }
+       
+        
+    }
+}
